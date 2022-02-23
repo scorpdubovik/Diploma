363e2becc7d395a5ecb2a0e4c29114f3ea15aef0
Andrey <scorpdubovik@gmail,com>
2022-02-23 23:29:14 +0300

    Diploma in work!

---
+++ src/test/java/baseEntity/BaseTest.java
@@ -0,0 +1,39 @@
+package baseEntity;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import core.ReadProperties;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class BaseTest {
+
+
+    @BeforeClass
+    public void setUp() {
+        //for fine-tuning:
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+                .screenshots(true)
+                .savePageSource(true)
+        );
+
+        // Настройка slf4j
+        org.apache.log4j.BasicConfigurator.configure();
+
+        // Настройка Selenide
+        Configuration.baseUrl = ReadProperties.getUrl();
+        Configuration.browser = ReadProperties.getBrowserName().toLowerCase();
+        Configuration.startMaximized = false;
+        Configuration.fastSetValue = true;
+        Configuration.timeout = 8000;
+        Configuration.headless = ReadProperties.isHeadless();
+    }
+
+    @AfterClass
+    public void closePage() {
+        closeWebDriver();
+    }
+}
